00c89c218ffc045b7cab1a26ab5fc64abc7f7461
Chernousov Anton <chernousov@interlan.xyz>
2024-10-11 14:06:13 +0700

    Variables

---
+++ module_1/module_1_4_variables.py
@@ -0,0 +1,7 @@
+#! /usr/bin/python
+
+home_works_count = 12
+hours_spent = 1.5
+course_name = 'Python'
+
+print ("Курс: "+course_name+", всего задач:"+str(home_works_count)+", затрачено часов: "+str(hours_spent)+", среднее время выполнения "+str(hours_spent/home_works_count)+" часа.")

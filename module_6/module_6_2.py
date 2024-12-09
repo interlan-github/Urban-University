474eea8731e35ebbb0459ab3eab82ba103d954b7
Chernousov Anton <chernousov@interlan.xyz>
2024-12-09 20:25:50 +0700

    module_6_2

--- module_6/module_6_2.py
+++ module_6/module_6_2.py
@@ -1,63 +1,61 @@
 #!/usr/bin/python3
 
-class Animal:
+class Vehicle:
     # Predefined values
-    alive = True
-    fed = False
-    name = None
-    # Init Animal
-    def __init__(self, data):
-        self.alive = data['alive']
-        self.name = data['name']
-        self.fed = data ['fed']
+    owner = None
+    __model = None
+    __engine_power = None
+    __color = None
+    def __init__(self, **kwargs):
+        self.owner = kwargs.get('owner')
+        self.__model = kwargs.get('model')
+        self.__engine_power = kwargs.get('engine_power')
+        self.__color = kwargs.get('color')
+        self.COLOR_VARIANTS = ['Желтый','Белый','Синий']
         pass
-
-class Plant:
-    # Predefined values
-    edible = False
-    name = None
-    # Init Plant
-    def __init__(self, data):
-        self.edible = data['edible']
-        self.name = data['name']
+    def get_owner(self):
+        return self.owner
+    def get_model(self):
+        return self.__model
+    def get_horsepower(self):
+        return self.__engine_power
+    def get_color(self):
+        if self.__color not in self.COLOR_VARIANTS:
+            print ('Такого цвета не бывает')
+        return self.__color
+    def print_info(self):
+        return 'Владелец: ' + self.owner + ' - Мощь в конях: ' + str (self.__engine_power) + ' - цвет: ' + self.__color
+    def set_color(self, new_color):
+        self.__color = new_color
+        print (self.COLOR_VARIANTS)
+        if self.__color not in self.COLOR_VARIANTS:
+            print ('Такого цвета не бывает')
         pass
+    
+print ('Первая машина')
+base_auto = Vehicle(owner = 'Иван', model = 'Запорожец', engine_power = 'Пердячья тяга', color = 'Желтый')
+print (base_auto.get_owner())
+print (base_auto.get_model())
+print (base_auto.get_horsepower())
+print (base_auto.get_color())
+print (base_auto.print_info())
+print ('------------------ \nВторая машина')
+base_auto_test = Vehicle(owner = 'Колян', model = 'Мерс', engine_power = 120, color = 'Странный')
+print (base_auto_test.get_color())
+print ('Перекрашиваем')
+print (base_auto_test.set_color('Желтый'))
+print (base_auto_test.print_info())
+print ('Перекрашиваем')
 
-class Mammal(Animal):
-    def eat (self, food):
-        print (f'Я {self.name} поел {food.name}')
-        if food.edible:
-            print ('Спасибо вкусно')
-        else:
-            print ('Усе, я помер')
-            self.alive = False
+# Еще одна тачка
+print ('------------------ \nЕще одна машина')
+class Sedan(Vehicle):
+    def __init__(self, **kwargs):
+        Vehicle.__init__(self, **kwargs)
+        self.COLOR_VARIANTS = ['blue', 'red', 'green', 'black', 'white']
+        self.PASSENGERS_LIMIT = 5
 
-class Predator(Animal):
-    def eat (self, food):
-        print (f'Я {self.name} поел {food.name}')
-        if food.edible:
-            print ('Спасибо вкусно')
-        else:
-            print ('Усе, я помер')
-            self.alive = False
-
-# Растения
-plant1 = Plant(data = {'edible':True,'name':'Помидорка'})
-print (plant1.edible, plant1.name)
-
-plant2 = Plant(data = {'edible':False,'name':'Мухомор'})
-print (plant2.edible, plant2.name)
-
-# Звери
-cat1 = Animal(data = {'alive':True,'name':'Муська','fed':True})
-print (cat1.alive, cat1.name, cat1.fed)
-
-cat1 = Mammal(data = {'alive':True,'name':'Муська','fed':True})
-cat1.eat (plant1)
-print (cat1.alive, cat1.name, cat1.fed)
-
-cat2 = Animal(data = {'alive':False,'name':'Дохлый бобер','fed':False})
-print (cat2.alive, cat2.name, cat2.fed)
-
-cat3 = Predator(data = {'alive':True,'name':'Тигра','fed':True})
-cat3.eat (plant2)
-print (cat3.alive, cat3.name, cat3.fed)
+my_new_car = Sedan(owner = 'Жека', model = 'Мерс', engine_power = 120, color = 'red')
+my_new_car.set_color('red')
+print (my_new_car.print_info())
+print (my_new_car.COLOR_VARIANTS)

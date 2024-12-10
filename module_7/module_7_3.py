89eeb83f5d8d9dcea1282fec271219d8faeaf5a1
Chernousov Anton <chernousov@interlan.xyz>
2024-12-10 16:16:10 +0700

    module_7_3

---
+++ module_7/module_7_3.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# Мне стало скучно и я сделал недо-полнотекстовый поиск
+import glob
+
+# Перебор всех txt-файлов в каталоге
+txt_file_list = (glob.glob('./module_7/test_files/*.txt', recursive=True))
+
+# Выкидываем всякие знаки
+replacer = [',', '.', '=', '!', '?', ';', ':', ' - ', ' ', '\n'] 
+
+# Строим словарь (первый проход)
+dictionary = []
+for file in txt_file_list:
+    with open(file, encoding='UTF8') as file:
+        for line in file:
+            new_line = line
+            for fixer in replacer:
+                line=line.replace(fixer, ' ')
+            for word in line.split(' '):
+                if word not in dictionary and word !='' and word !=' ':
+                    dictionary.append(word.lower())
+
+print ('Чистый словарь уникальных слов:' + str(dictionary))
+
+# Второй проход поиск по тексту
+search_word = 'solr'
+for file in txt_file_list:
+    with open(file, encoding='UTF8') as file:
+        for line in file:
+            lower_line = line.lower()
+            if lower_line.find('solr') != -1:
+                print (f'Нашли {search_word} в файле {str(file)}')

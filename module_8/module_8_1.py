274a668d75304952de905f1ea395b63317e6d65d
Chernousov Anton <chernousov@interlan.xyz>
2024-12-17 13:12:44 +0700

    module-8-1

---
+++ module_8/module_8_1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def add_everything_up(arg1,arg2):
+    try:
+        result = round(arg1 + arg2, 3)
+    except TypeError:
+        result = str(arg1) + str(arg2)
+    return result
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
